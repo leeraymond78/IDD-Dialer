4df47f4a5af2033505f904758084e4cb65e6b128
Raymond Lee <leeraymond@Raymonds-MacBook-Pro-3.local>
2013-06-29 01:25:01 +0800

    First

---
+++ IDD Dialer/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IDD Dialer
+//
+//  Created by Raymond Lee on 29/6/13.
+//  Copyright (c) 2013 RayCom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ IDD Dialer/MainViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IDD Dialer
+//
+//  Created by Raymond Lee on 29/6/13.
+//  Copyright (c) 2013 RayCom. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
