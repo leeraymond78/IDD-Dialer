3c5055541111e782849b590fbb54fc8879dd40a7
Raymond Lee <leeraymond@Raymonds-MacBook-Pro-3.local>
2013-06-29 01:21:29 +0800

    Initial Commit

---
+++ IDD Dialer/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  IDD Dialer
+//
+//  Created by Raymond Lee on 29/6/13.
+//  Copyright (c) 2013 RayCom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ IDD Dialer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IDD Dialer
+//
+//  Created by Raymond Lee on 29/6/13.
+//  Copyright (c) 2013 RayCom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
